091ba779c57962c7fe1b4fb674946f5475be5ec6
Justin Tamargo <justin_tamargo22@yahoo.com>
2019-05-09 23:40:59 +0800

    update logo

--- facilities.php
+++ facilities.php
@@ -19,7 +19,7 @@ include "database/config.php";
         <!-- Navigation -->
         <nav class="navbar navbar-default navbar-static-top m-b-0">
             <div class="navbar-header"> <a class="navbar-toggle hidden-sm hidden-md hidden-lg " href="javascript:void(0)" data-toggle="collapse" data-target=".navbar-collapse"><i class="fa fa-bars"></i></a>
-                <div class="top-left-part"><a class="logo" href="index.html"><b>iRequest</b><span class="hidden-xs"></span></a></div>
+                <div class="top-left-part"><a class="logo" >i<b>Request</b><span class="hidden-xs"></span></a></div>
                 <ul class="nav navbar-top-links navbar-left m-l-20 hidden-xs">
                     <!-- <li>
                         <form role="search" class="app-search hidden-xs">
