3f3215497727a8f7d1463deecc84946286021669
Justin Tamargo <justin_tamargo22@yahoo.com>
2019-05-09 23:34:54 +0800

    departments

--- delete_department.php
+++ delete_department.php
@@ -6,10 +6,10 @@
     $department_id=$_GET['department_id'];
 
     if(updateDatabase($conn, "DELETE FROM `department_tb` WHERE department_id='$department_id'") == 1){
-        redirectPageWithAlert("user_management.php", "User Account Successfully Deleted");
+        redirectPageWithAlert("department.php", "User Account Successfully Deleted");
     }else{
         //echo updateDatabase($conn, "INSERT into department_tb(department_name, department_description) VALUES ('$department_name', '$department_description')");
-        redirectPageWithAlert("user_management.php", "Error. Please Try Again");
+        redirectPageWithAlert("department.php", "Error. Please Try Again");
     }
 
 ?>
