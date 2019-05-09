655f468b2ad96cd086b5e544b8ba73893a288fcb
Justin Tamargo <justin_tamargo22@yahoo.com>
2019-05-09 22:58:56 +0800

    last try - id

--- controllers/users_functions.php
+++ controllers/users_functions.php
@@ -20,7 +20,6 @@ if(!function_exists('getUsersData')){
         }else{
             if(mysqli_num_rows($query)>0){
                 while($row = mysqli_fetch_array($query)){
-                    $id = $row['user_id'];
                     ?>
                      <tr>
                         <td class="txt-oflo"><?php echo $row['first_name'] ?></td>
@@ -32,7 +31,7 @@ if(!function_exists('getUsersData')){
                         <td class="txt-oflo"><?php echo getDepartmentDetailsByDepartmentId($conn, $row['department_id'])[2]; ?></td>
                         <td class="txt-oflo"><?php echo $row['role_type'] ?></td>
                         <td class="txt-oflo"><?php echo $row['user_status'] ?></td>
-                        <td class="txt-oflo"><a href = "delete_user.php<?php echo '?id='.$id; ?>">Delete</a></td>
+                        <td class="txt-oflo"><a href = "delete_user.php<?php echo '?id='.$row['user_id'] ?>">Delete</a></td>
                     </tr>
                     <?php
                 }
