30c3effaca9ce7faf9e48244e3bd8191497c6020
Justin Tamargo <justin_tamargo22@yahoo.com>
2019-05-09 23:13:22 +0800

    added delete in user management

--- delete_user.php
+++ delete_user.php
@@ -5,7 +5,7 @@
 
     $get_id=$_GET['id'];
 
-    if(updateDatabase($conn, "DELETE FROM `user_tb` WHERE user_id='$get_id'") == 1){
+    if(updateDatabase($conn, "DELETE FROM `users_tb` WHERE user_id='$get_id'") == 1){
         redirectPageWithAlert("user_management.php", "User Account Successfully Deleted");
     }else{
         //echo updateDatabase($conn, "INSERT into department_tb(department_name, department_description) VALUES ('$department_name', '$department_description')");
