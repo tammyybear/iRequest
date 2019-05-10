31df684aa33f3037e2412bf6e99ad1ef3e477cae
Justin Tamargo <justin.tamargo@dss.asia>
2019-05-10 11:49:16 +0800

    update facility form

--- facilities.php
+++ facilities.php
@@ -61,20 +61,20 @@ include "database/config.php";
                 <div class="row">
                     <div class="col-md-8 col-xs-12">
                         <div class="white-box">
-                            <form class="form-horizontal form-material" method="post" action="add_department.php">
+                            <form class="form-horizontal form-material" method="post" action="add_facility.php">
                                 <div class="form-group">
-                                    <label class="col-md-12">Department Name</label>
+                                    <label class="col-md-12">Facility Name</label>
                                     <div class="col-md-12">
-                                        <input type="text" placeholder="Department Name" name="department_name" class="form-control form-control-line" required> </div>
+                                        <input type="text" placeholder="Facility Name" name="facility_name" class="form-control form-control-line" required> </div>
                                 </div>
                                 <div class="form-group">
-                                    <label for="example-email" class="col-md-12">Department Description</label>
+                                    <label for="example-email" class="col-md-12">Facility Description</label>
                                     <div class="col-md-12">
-                                        <input type="text" placeholder="Department Description" class="form-control form-control-line" name="department_description" id="example-email" required> </div>
+                                        <input type="text" placeholder="Facility Description" class="form-control form-control-line" name="facility_description" required> </div>
                                 </div>
                                 <div class="form-group">
                                     <div class="col-sm-12">
-                                        <button class="btn btn-success">Add Department</button>
+                                        <button class="btn btn-success">Add Facility</button>
                                     </div>
                                 </div>
                             </form>
@@ -84,14 +84,13 @@ include "database/config.php";
                 <div class="row">
                     <div class="col-sm-12">
                         <div class="white-box">
-                            <h3 class="box-title">Recent sales
+                            <h3 class="box-title">
                             <div class="table-responsive">
                                 <table class="table ">
                                     <thead>
                                         <tr>
-                                            <th>Department Name</th>
-                                            <th>Department Description</th>
-                                            <th>Number of Members</th>
+                                            <th>Facility Name</th>
+                                            <th>Facility Description</th>                                            
                                             <th>Update</th>
                                             <th>Delete</th>
                                         </tr>
@@ -99,7 +98,8 @@ include "database/config.php";
                                     <tbody>
                                       <?php getDepartmentData($conn); ?>
                                     </tbody>
-                                </table> <a href="#">Check all the sales</a> </div>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
