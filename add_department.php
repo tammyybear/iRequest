a31f39f00354c95bb4fa747b3a5ebc6e40473e41
Justin Tamargo <justin.tamargo@dss.asia>
2019-05-10 11:56:10 +0800

    debug department page

--- add_department.php
+++ add_department.php
@@ -7,9 +7,9 @@ $department_name = $_POST['department_name'];
 $department_description = $_POST['department_description'];
 
 if(updateDatabase($conn, "INSERT into department_tb(department_name, department_description) VALUES ('$department_name', '$department_description')") == 1){
-    redirectPageWithAlert("departments.php", "Department Successfully Added");
+    redirectPageWithAlert("department.php", "Department Successfully Added");
 }else{
     //echo updateDatabase($conn, "INSERT into department_tb(department_name, department_description) VALUES ('$department_name', '$department_description')");
-    redirectPageWithAlert("departments.php", "Error. Please Try Again");
+    redirectPageWithAlert("department.php", "Error. Please Try Again");
 }
 ?>
--- delete_department.php
+++ delete_department.php
@@ -6,7 +6,7 @@
     $department_id=$_GET['department_id'];
 
     if(updateDatabase($conn, "DELETE FROM `department_tb` WHERE department_id='$department_id'") == 1){
-        redirectPageWithAlert("department.php", "User Account Successfully Deleted");
+        redirectPageWithAlert("department.php", "Department Successfully Deleted");
     }else{
         //echo updateDatabase($conn, "INSERT into department_tb(department_name, department_description) VALUES ('$department_name', '$department_description')");
         redirectPageWithAlert("department.php", "Error. Please Try Again");
