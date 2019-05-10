5643b47bae95a3621e26a59dc8bb5b51705a8a5f
Justin Tamargo <justin.tamargo@dss.asia>
2019-05-10 08:31:33 +0800

    updates

--- controllers/users_functions.php
+++ controllers/users_functions.php
@@ -25,13 +25,13 @@ if(!function_exists('getUsersData')){
                         <td class="txt-oflo"><?php echo $row['first_name'] ?></td>
                         <td><?php echo $row['middle_name'] ?></td>
                         <td class="txt-oflo"><?php echo $row['last_name'] ?></td>
-                        <td class="txt-oflo"><?php echo $row['name_extension'] ?></td>g
+                        <td class="txt-oflo"><?php echo $row['name_extension'] ?></td>
                         <td class="txt-oflo"><?php echo $row['mobile_number'] ?></td>
                         <td class="txt-oflo"><?php echo $row['address'] ?></td>
                         <td class="txt-oflo"><?php echo getDepartmentDetailsByDepartmentId($conn, $row['department_id'])[2]; ?></td>
                         <td class="txt-oflo"><?php echo $row['role_type'] ?></td>
                         <td class="txt-oflo"><?php echo $row['user_status'] ?></td>
-                        <td class="txt-oflo"><a href = "delete_user.php<?php echo '?id='.$row['user_id']; ?>">Delete</a></td>
+                        <td class="txt-oflo"><a href = "delete_user.php?user_id = <?php echo $row['user_id'] ?>">Delete</a></td>
                     </tr>
                     <?php
                 }
