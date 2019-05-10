259725905e5d834e1eb4a77fcb42a7e003eb0346
Justin Tamargo <justin.tamargo@dss.asia>
2019-05-10 11:02:30 +0800

    update breadcrubms

--- facilities.php
+++ facilities.php
@@ -50,7 +50,7 @@ include "database/config.php";
             <div class="container-fluid">
                 <div class="row bg-title">
                     <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
-                        <h4 class="page-title">Departments</h4> </div>
+                        <h4 class="page-title">Facilities</h4> </div>
                     <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
                         <ol class="breadcrumb">
                             <li class="active">Facilities</li>
