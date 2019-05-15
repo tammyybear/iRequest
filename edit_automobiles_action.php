43f43147e8d79797d90f4fc1a62976a3b51f438c
Justin Tamargo <justin.tamargo@dss.asia>
2019-05-15 10:40:01 +0800

    finish CRUD part

---
+++ edit_automobiles_action.php
@@ -0,0 +1,17 @@
+<?php    
+    include "controllers/database_functions.php";
+    include "controllers/basic_functions.php";
+    include "database/config.php";
+
+    $get_id=$_GET['id'];
+    $automobile_name = $_POST['automobile_name'];
+    $automobile_plate = $_POST['automobile_plate'];
+
+    if(updateDatabase($conn, "UPDATE inventory_items_tb set item_name = '$automobile_name', item_description = '$automobile_plate' where inventory_item_id ='$get_id'") == 1){
+        redirectPageWithAlert("automobiles.php", "Automobile Successfully Updated");
+    }else{
+        //echo updateDatabase($conn, "INSERT into department_tb(department_name, department_description) VALUES ('$department_name', '$department_description')");
+        redirectPageWithAlert("automobiles.php", "Error. Please Try Again");
+    }
+
+?>
