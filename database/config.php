1d93c02fe329dfdd6a4db9c298c984e91614fddc
Justin Tamargo <justin.tamargo@dss.asia>
2019-05-15 13:15:04 +0800

    add session to all pages

--- database/config.php
+++ database/config.php
@@ -1,4 +1,5 @@
 <?php
+include "controllers/basic_functions.php";
 
 $servername = "remotemysql.com:3306";
 $dbusername = "M5vKHHE91E";
@@ -9,6 +10,10 @@ $conn = mysqli_connect($servername, $dbusername, $dbpassword, $dbname);
 
 if (!$conn) {
    die("connection failed: ".mysqli_connect_error());
+}
+
+if(!isset($_SESSION['role_type'])) {
+    redirectPageWithAlert("index.php", "Please login to continue");
 }
 
 ?>
--- edit_user_status.php
+++ edit_user_status.php
@@ -1,15 +1,25 @@
 <?php
     include "controllers/database_functions.php";
     include "controllers/basic_functions.php";
+    include "controllers/users_functions.php";
     include "database/config.php";
 
     $get_id=$_GET['id'];
 
-    if(updateDatabase($conn, "UPDATE users_tb set user_status = 'Inactive' where user_id='$get_id'") == 1){
-        redirectPageWithAlert("user_management.php", "User Status Successfully Updated");
+    if(getUserDetailsById($conn, $get_id)[10] == "Active") {
+        if(updateDatabase($conn, "UPDATE users_tb set user_status = 'Inactive' where user_id='$get_id'") == 1){
+            redirectPageWithAlert("user_management.php", "User Status Successfully Updated");
+        }else{        
+            redirectPageWithAlert("user_management.php", "Error. Please Try Again");
+        }
     }else{
-        //echo updateDatabase($conn, "INSERT into department_tb(department_name, department_description) VALUES ('$department_name', '$department_description')");
-        redirectPageWithAlert("user_management.php", "Error. Please Try Again");
+        if(updateDatabase($conn, "UPDATE users_tb set user_status = 'Active' where user_id='$get_id'") == 1){
+            redirectPageWithAlert("user_management.php", "User Status Successfully Updated");
+        }else{        
+            redirectPageWithAlert("user_management.php", "Error. Please Try Again");
+        }
     }
+
+    
 
 ?>
