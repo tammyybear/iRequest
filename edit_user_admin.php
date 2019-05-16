0dc1cf1a877a7319e8aa4b062f0f4735dc0dbeae
Justin Tamargo <justin.tamargo@dss.asia>
2019-05-16 15:11:32 +0800

    start edit user

---
+++ edit_user_admin.php
@@ -0,0 +1,77 @@
+<?php
+include "controllers/include_partial_functions.php";
+include "controllers/users_functions.php";
+include "database/config.php";
+include "controller/check_login.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <?php get_headBlade(); ?>
+</head>
+
+<body>
+    <!-- Preloader -->
+    <div class="preloader">
+        <div class="cssload-speeding-wheel"></div>
+    </div>
+    <div id="wrapper">
+        <!-- Navigation -->
+        <nav class="navbar navbar-default navbar-static-top m-b-0">
+            <div class="navbar-header">
+                <a class="navbar-toggle hidden-sm hidden-md hidden-lg " href="javascript:void(0)" data-toggle="collapse" data-target=".navbar-collapse">
+                    <i class="fa fa-bars"></i>
+                </a>
+                <div class="top-left-part">
+                    <a class="logo" alt="logo">
+                        <b><img src="resources/images/logo.png"/></b>
+                        <small class="hidden-xs"><b>request</b></small>
+                    </a>
+                </div>
+                <ul class="nav navbar-top-links navbar-right pull-right">
+                    <li>                                           
+                        <?php getHeaderUserName(); ?>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-header -->
+            <!-- /.navbar-top-links -->
+            <!-- /.navbar-static-side -->
+        </nav>
+        <!-- Left navbar-header -->
+       <?php 
+        get_NavBlade();
+       ?>
+        <!-- Left navbar-header end -->
+        <!-- Page Content -->
+        <div id="page-wrapper">
+            <div class="container-fluid">
+                <div class="row bg-title">
+                    <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
+                        <h4 class="page-title">User Management</h4> </div>
+                    <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
+                        <ol class="breadcrumb">
+                            <li><a href="user_management.php">User Management</a></li>
+                            <li class="active">Add User</li>
+                        </ol>
+                    </div>
+                    <!-- /.col-lg-12 -->
+                </div>
+                
+               <?php getAddUserForm($conn); ?>
+              
+                <!-- /.row -->
+            </div>
+            <!-- /.container-fluid -->
+            <?php get_FooterBlade(); ?>
+        </div>
+        <!-- /#page-wrapper -->
+    </div>
+    <!-- /#wrapper -->
+  <?php 
+    get_JSBlade();
+  ?>
+</body>
+
+</html>
