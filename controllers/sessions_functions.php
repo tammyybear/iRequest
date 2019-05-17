b7110d14ce4fa20227d49fe9be178b80c098be7d
Justin Tamargo <justin.tamargo@dss.asia>
2019-05-17 15:46:34 +0800

    services add function

--- controllers/sessions_functions.php
+++
@@ -1,6 +0,0 @@
-<?php
-include "basic_functions.php";
-if(!isset($_SESSION['role_type'])) {
-    redirectPageWithAlert("index.php", "Please login to continue");
-}
-?>
--- services_user_action.php
+++ services_user_action.php
@@ -4,22 +4,17 @@ include "database/config.php";
 include "controllers/database_functions.php";
 include "controllers/basic_functions.php";
 include "controllers/services_functions.php";
+include "controllers/users_functions.php";
 include "controllers/check_login.php";
 
-$user_id = $_SESSION['users_id'];
+$users_id = getUserDetailsByUsername($conn, $_SESSION['user'])[11];
 $request_subject = $_POST['request_subject'];
 $request_description = $_POST['request_description'];
 $ticketId = get_TicketId($conn);
 
-
-
-// if(countResult($conn, "SELECT * from inventory_items_tb where item_name = '$automobile_name'") == 1){
-//     redirectPageWithAlert("faciautomobileslities.php", "Error. Please Try Again");
-// }else{
-//     if(updateDatabase($conn, "INSERT into inventory_items_tb(item_name, item_description, inventory_cat_id) VALUES ('$automobile_name', '$automobile_plate','2')") == 1){
-//         redirectPageWithAlert("automobiles.php", "User Successfully Added");
-//     }else{
-//         redirectPageWithAlert("automobiles.php", "Error. Please Try Again");
-//     }
-// }
+if(updateDatabase($conn, "INSERT into services_tb (users_id, request_subject, request_description, request_status, ticket_id) VALUES ('$users_id', '$request_subject', '$request_description', 'Open', '$ticketId')") == 1){
+    redirectPageWithAlert("services_user.php", "Request Successfully Added. Here is your Ticket ID: $ticketId");
+}else{
+    redirectPageWithAlert("services_user.php", "Error. Please Try Again");
+}
 ?>
