c658d8b302c984f265dbd4aae7e552b9e9b21b72
Justin Tamargo <justin_tamargo22@yahoo.com>
2019-05-23 21:29:18 +0800

    update service action in mobile

--- mobile_app/services_user_action_mobile.php
+++ mobile_app/services_user_action_mobile.php
@@ -1,23 +1,33 @@
 <?php
 session_start();
+include "../database/config.php";
 include "../controllers/database_functions.php";
 include "../controllers/basic_functions.php";
-include "../database/config.php";
+include "../controllers/services_functions.php";
+include "../controllers/users_functions.php";
 include "../controllers/check_login.php";
 
-$ticketId = get_TicketId();
+if($_SESSION['role_type'] == "Admin"){
+    $users_id = 0;
+}else{
+    $users_id = getUserDetailsByUsername($conn, $_SESSION['user'])[11];
+}
+
 $request_subject = $_POST['request_subject'];
 $request_description = $_POST['request_description'];
-$users_id = getUserDetailsByUsername($conn, $_SESSION['user']);
+$ticketId = get_TicketId($conn);
 
-
-if(countResult($conn, "SELECT * from services_tb where request_subject = '$request_subject' and request_description = '$request_description'") == 1){
-        redirectPageWithAlert("services_user_mobile.php", "Error. Please Try Again");
+if(updateDatabase($conn, "INSERT into services_tb (users_id, request_subject, request_description, request_status, ticket_id) VALUES ('$users_id', '$request_subject', '$request_description', 'Open', '$ticketId')") == 1){
+    if($_SESSION['role_type'] == "Admin"){
+        redirectPageWithAlert("services_admin.php", "Request Successfully Added. Here is your Ticket ID: $ticketId");
+    }else{
+        redirectPageWithAlert("services_user_mobile.php", "Request Successfully Added. Here is your Ticket ID: $ticketId");
+    }    
 }else{
-    if(updateDatabase($conn, "INSERT into services_tb(users_id, request_subject, request_description, ticket_id) VALUES ('$users_id', '$request_subject', '$request_description', '$ticketId')") == 1){
-        redirectPageWithAlert("services_user_mobile.php", "Request Successfully Added");
+    if($_SESSION['role_type'] == "Admin"){
+        redirectPageWithAlert("services_admin.php", "Error. Please Try Again");
     }else{
         redirectPageWithAlert("services_user_mobile.php", "Error. Please Try Again");
-    }
+    }    
 }
 ?>
