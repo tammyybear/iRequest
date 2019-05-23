48333873f294e52570889b68b170954f1c327522
Justin Tamargo <justin_tamargo22@yahoo.com>
2019-05-23 21:25:49 +0800

    push back service

--- controllers/services_functions.php
+++ controllers/services_functions.php
@@ -1,12 +1,28 @@
 <?php
-include "database/config.php";
-include "controllers/check_login.php";
-include "controllers/database_functions.php";
+include "database_functions.php";
 
 if(!function_exists('get_TicketId')){
-    function get_TicketId(){
-        $TicketId = uniqid('iRequest_', true);
+    function get_TicketId($conn){
+        $keyLength = 8;
+        $strings = "123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        $TicketId = substr(str_shuffle($strings), 0, $keyLength);
+        $CheckTicketId = CheckTicketId($conn, $TicketId);
+        while ($CheckTicketId == true) {
+            $TicketId = substr(str_shuffle($strings), 0, $keyLength);
+            $CheckTicketId = CheckTicketId($conn, $TicketId);
+        }
         return $TicketId;
+    }
+}
+
+if(!function_exists('CheckTicketId')){
+    function CheckTicketId($conn, $TicketId){
+        if(countResult($conn, "SELECT * from services_tb where ticket_id = '$TicketId'") == 1){
+            $keyExists = true;
+        }else{
+            $keyExists = false;
+        }
+        return $keyExists;
     }
 }
 
--- services_user_action.php
+++ services_user_action.php
@@ -1,40 +1,33 @@
 <?php
 session_start();
+include "database/config.php";
 include "controllers/database_functions.php";
 include "controllers/basic_functions.php";
-include "database/config.php";
-include "controllers/check_login.php";
 include "controllers/services_functions.php";
 include "controllers/users_functions.php";
+include "controllers/check_login.php";
 
-$ticketId = get_TicketId();
-$request_subject = $_POST['request_subject'];
-$request_description = $_POST['request_description'];
 if($_SESSION['role_type'] == "Admin"){
     $users_id = 0;
 }else{
-    $users_id = getUserDetailsByUsername($conn, $_SESSION['user']);
+    $users_id = getUserDetailsByUsername($conn, $_SESSION['user'])[11];
 }
 
-if(countResult($conn, "SELECT * from services_tb where request_subject = '$request_subject' and request_description = '$request_description'") == 1){
+$request_subject = $_POST['request_subject'];
+$request_description = $_POST['request_description'];
+$ticketId = get_TicketId($conn);
+
+if(updateDatabase($conn, "INSERT into services_tb (users_id, request_subject, request_description, request_status, ticket_id) VALUES ('$users_id', '$request_subject', '$request_description', 'Open', '$ticketId')") == 1){
+    if($_SESSION['role_type'] == "Admin"){
+        redirectPageWithAlert("services_admin.php", "Request Successfully Added. Here is your Ticket ID: $ticketId");
+    }else{
+        redirectPageWithAlert("services_user.php", "Request Successfully Added. Here is your Ticket ID: $ticketId");
+    }    
+}else{
     if($_SESSION['role_type'] == "Admin"){
         redirectPageWithAlert("services_admin.php", "Error. Please Try Again");
     }else{
         redirectPageWithAlert("services_user.php", "Error. Please Try Again");
-    }
-}else{
-    if(updateDatabase($conn, "INSERT into services_tb(users_id, request_subject, request_description, ticket_id) VALUES ('$users_id', '$request_subject', '$request_description', '$ticketId')") == 1){
-        if($_SESSION['role_type'] == "Admin"){
-            redirectPageWithAlert("services_admin.php", "Request Successfully Added");
-        }else{
-            redirectPageWithAlert("services_user.php", "Request Successfully Added");
-        }
-    }else{
-        if($_SESSION['role_type'] == "Admin"){
-            redirectPageWithAlert("services_admin.php", "Error. Please Try Again");
-        }else{
-            redirectPageWithAlert("services_user.php", "Error. Please Try Again");
-        }
-    }
+    }    
 }
 ?>
