77d3d1ff394ebca0634eeb5f8c3d0f6a7f5c741e
Justin Tamargo <justin.tamargo@dss.asia>
2019-05-23 17:57:16 +0800

    debug services mobile

--- mobile_app/services_user_action_mobile.php
+++ mobile_app/services_user_action_mobile.php
@@ -5,18 +5,19 @@ include "../controllers/basic_functions.php";
 include "../database/config.php";
 include "../controllers/check_login.php";
 
-$automobile_name = $_POST['request_subject'];
-$automobile_plate = $_POST['request_description'];
+$ticketId = get_TicketId();
+$request_subject = $_POST['request_subject'];
+$request_description = $_POST['request_description'];
+$users_id = getUserDetailsByUsername($conn, $_SESSION['user']);
 
 
-
-if(countResult($conn, "SELECT * from inventory_items_tb where item_name = '$automobile_name'") == 1){
-    redirectPageWithAlert("faciautomobileslities.php", "Error. Please Try Again");
+if(countResult($conn, "SELECT * from services_tb where request_subject = '$request_subject' and request_description = '$request_description'") == 1){
+        redirectPageWithAlert("services_user_mobile.php", "Error. Please Try Again");
 }else{
-    if(updateDatabase($conn, "INSERT into inventory_items_tb(item_name, item_description, inventory_cat_id) VALUES ('$automobile_name', '$automobile_plate','2')") == 1){
-        redirectPageWithAlert("automobiles.php", "User Successfully Added");
+    if(updateDatabase($conn, "INSERT into services_tb(users_id, request_subject, request_description, ticket_id) VALUES ('$users_id', '$request_subject', '$request_description', '$ticketId')") == 1){
+        redirectPageWithAlert("services_user_mobile.php", "Request Successfully Added");
     }else{
-        redirectPageWithAlert("automobiles.php", "Error. Please Try Again");
+        redirectPageWithAlert("services_user_mobile.php", "Error. Please Try Again");
     }
 }
 ?>
