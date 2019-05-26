68ce2c4623c528bf1ce23688b57b0bffee1fca19
unknown <justamargo@gmail.com>
2019-05-26 14:21:52 +0800

    last push

--- reservations_admin.php
+++ reservations_admin.php
@@ -51,10 +51,10 @@ include "controllers/bookings_functions.php";
             <div class="container-fluid">
                 <div class="row bg-title">
                     <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
-                        <h4 class="page-title">Services</h4> </div>
+                        <h4 class="page-title">Reservations</h4> </div>
                     <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
                         <ol class="breadcrumb">
-                            <li class="active">Services</li>
+                            <li class="active">Reservations</li>
                         </ol>
                     </div>
                 </div>
