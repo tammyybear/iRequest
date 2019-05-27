53b3661990f0a20df1cb6c9417105090a7b7fa3c
Justin Tamargo <justin.tamargo@dss.asia>
2019-05-27 11:32:15 +0800

    debug

--- reservations_admin_action.php
+++ reservations_admin_action.php
@@ -11,11 +11,13 @@ $get_item = $_GET['item'];
 $get_from = $_GET['from'];
 $get_to = $_GET['to'];
 
+exit();
+
 if($get_checker == "1"){
 
-    if(countResult ($conn, "SELECT * from bookings_tb where status = 'Approve' AND inventory_item_id = '$get_item' AND date_from_requested between '$get_from' and '$get_to' AND date_to_requested between '$get_from' and '$get_to' ") == 1){
+    if(countResult ($conn, "SELECT * from bookings_tb where status = 'Approve' and inventory_item_id = '$get_item' and date_from_requested BETWEEN '$get_from' and '$get_to' or date_to_requested BETWEEN '$get_from' and '$get_to' ") == 1){
         updateDatabase($conn, "UPDATE bookings_tb set status = 'Disapprove' where status = 'Pending' and inventory_item_id = '$get_item' and date_from_requested = '$get_from' and date_to_requested = '$get_to'");
-        redirectPageWithAlert("reservations_admin.php", "There is already approved reservation for that");
+        redirectPageWithAlert("reservations_admin.php", "There is already approved reservation for that. The reservation was disapproved");
     }else{
         if(countResult($conn, "SELECT * from bookings_tb where date_from_requested = '$get_from' or date_to_requested = '$get_to' and inventory_item_id = '$get_item' and status = 'Pending'") == 1) {
             if(updateDatabase($conn, "UPDATE bookings_tb set status = 'Approve' where booking_id ='$get_id'") == 1){
